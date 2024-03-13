491f19ad8e9b30ac0adf893bd5a05ebab66f0d8b
maducuer <maducuer@gmail.com>
2024-03-13 10:38:42 -0400

    Aula 1203
    estilização da aula do Marcelino

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./globals.css";
 
 export default function App() {
 
   return(
    <main>
         <Header/>
-        <Header/>
-        <Header/>
+        <Footer/>
+    
       </main>
   );
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header(){
     return(
         <header>
-        <h1>Header</h1>
+        <h2>Madu</h2>
         </header>
     );
 }
