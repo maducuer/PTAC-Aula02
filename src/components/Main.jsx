53293a1e63b78205c0afd937646042fe2dfa9293
maducuer <maducuer@gmail.com>
2024-03-27 16:09:56 -0400

    lista
    falta terminar

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,11 +3,26 @@ import { useState } from "react";
 export default function Main(){
 
     const [nome, setNome] = useState ("");
-    const [telefone, setTelefone] = useState ("");
+    const [telefone, setTelefone] = useState ();
+    const [listaContatos, setContatos] = useState([]);
+    const [idade, setIdade] = useState ();
+
+    const registrar = (event) => {
+        event.preventDefault();
+        alert("Deu Certo!");
+        setContatos([...listaContatos,
+            {
+            nomeSalvo: nome,
+            telefoneSalvo: telefone,
+            idadeSalvo: idade
+        }
+    ]);
+    }
+    console.table(listaContatos);
 
     return(
         <main>
-            <form>
+            <form onSubmit={registrar}>
                 <label htmlFor="nome">Nome:</label>
                 <input type="text" name="" id="" onChange={(event) => setNome (event.target.value)}/>
                 {nome}
@@ -16,6 +31,11 @@ export default function Main(){
                 <label htmlFor="telefone">Telefone:</label>
                 <input type="tel" id="Telefone" value={telefone} onChange={(event) => setTelefone (event.target.value)}/>
                 {telefone}
+             <br></br>
+             <br></br>
+             <label htmlFor="idade">Idade:</label>
+                <input type="number" id="idade" value={idade} onChange={(event) => setIdade (event.target.value)}/>
+                {idade}
              <br></br>
              <br></br>
                 <button type="submit">Salvar</button>
