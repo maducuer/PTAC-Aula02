8f94e8f7fcef40d6fcae042d7104d7d64e467352
maducuer <maducuer@gmail.com>
2024-03-27 08:52:07 -0400

    botão
    botão feito

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,9 +11,14 @@ export default function Main(){
                 <label htmlFor="nome">Nome:</label>
                 <input type="text" name="" id="" onChange={(event) => setNome (event.target.value)}/>
                 {nome}
+                <br></br>
+             <br></br>
                 <label htmlFor="telefone">Telefone:</label>
                 <input type="tel" id="Telefone" value={telefone} onChange={(event) => setTelefone (event.target.value)}/>
                 {telefone}
+             <br></br>
+             <br></br>
+                <button type="submit">Salvar</button>
             </form>
         </main>
     );
